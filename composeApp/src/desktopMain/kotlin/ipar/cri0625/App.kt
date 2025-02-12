affad691c1256190fa599bd31b9eee0a895ca671
nayrariosaguilar <nayrios2004@gmail.com>
2025-02-12 20:28:23 +0100

    select y join?

--- composeApp/src/desktopMain/kotlin/ipar/cri0625/App.kt
+++ composeApp/src/desktopMain/kotlin/ipar/cri0625/App.kt
@@ -64,10 +64,14 @@ fun App(sqlDriver: SqlDriver) {
         loginUser(databaseSqlite)
     }
 
+    val username = "nayrios2004"
+    val password = "Adri888"
+
     DisposableEffect(Unit) {
         CoroutineScope(Dispatchers.Default).launch {
+            val isValidUser = validateUser(databaseSqlite, username, password)
             try {
-                val connectionString = "mongodb+srv://nayrios2004:Adri888@prueba.7rxv1.mongodb.net/?retryWrites=true&w=majority&appName=Prueba"
+                val connectionString = "mongodb+srv://:Adri888@prueba.7rxv1.mongodb.net/?retryWrites=true&w=majority&appName=Prueba"
                 database = setupConnection(connectionString, "test")
             } catch (me: MongoException) {
                 error = me.message
@@ -86,6 +90,10 @@ fun App(sqlDriver: SqlDriver) {
     }
 }
 
+suspend fun logout(db: Database){
+    //db.close
+}
+
 @Serializable
 object HomeRoute
 @Serializable
@@ -97,6 +105,11 @@ object VerURL
 
 fun loginUser(db: Database){
     db.loginQueries.insert("nayrios2004", "Adri888")
+}
+
+fun validateUser(db: Database, username: String, password: String): Boolean {
+    val result = db.loginQueries.select(username).executeAsOneOrNull()
+    return result != null && result.password == password
 }
 
 data class URL(
@@ -118,215 +131,6 @@ suspend fun setupConnection(connectionString: String, databaseName: String): Mon
     }
     return database
 }
-//INSERTA VER URLS
-@Composable
-fun ButacaScreen(db: Database, controller: NavController) {
-    val butacas = db.butacaQueries.select().executeAsList()
-    Column {
-        Nav(controller)
-        LazyColumn {
-            items(butacas) { butaca ->
-                Text("La numero de butaca es: ")
-                Text(butaca.numero.toString())
-            }
-        }
-
-    }
-//}
-//@Composable
-//fun SessionScreen(db: Database, controller: NavController){
-//    val sessions = db.sessionQueries.select().executeAsList()
-//    Column {
-//        Nav(controller)
-//        LazyColumn {
-//            items(sessions){ session ->
-//               Text("el dia de la session es: ")
-//                Text(session.dia.toString())
-//            } }
-//    }
-//}
-//
-//@Composable
-//fun PrecioScreen(db: Database, controller: NavController){
-//
-//    val preus = db.preuQueries.select().executeAsList()
-//
-//    Column {
-//        Nav(controller)
-//        LazyColumn {
-//            items(preus){ preu ->
-//                Text("el precio de tu espectaculo es: ")
-//                Text(preu.preu)
-//            } }
-//    }
-//}
-//
-//@Composable
-//fun CompraEntradaScreen(db: Database, controller: NavController){
-//    val zonas = db.zonaQueries.select().executeAsList()
-//    val espectacles= db.espectacleQueries.select().executeAsList()
-//    val preus = db.preuQueries.select().executeAsList()
-//    //AQUI PONDRE LA INFORMACIÓN DE LA ENTRADA
-//    val entradas = db.entradaQueries.select().executeAsList()
-//
-//    Column {
-//        Nav(controller)
-//
-//
-//        LazyColumn {
-//            items(entradas){ entrada ->
-//                Text("La numero de TU butaca es: ")
-//                Text(entrada.butaca.toString())
-//                Text("La numero de TU session es: ")
-//                Text(entrada.sessio.toString())
-//            } }
-//        LazyColumn {
-//            items(espectacles){ espectacle ->
-//                Text("Detalles del espectaculo ")
-//                Text(espectacle.nom)
-//                Text(espectacle.description.toString())
-//            } }
-//        LazyColumn {
-//            items(preus){ preu ->
-//                Text("el precio de tu espectaculo es: ")
-//                Text(preu.preu)
-//            } }
-//        LazyColumn {
-//            items(zonas){ zona ->
-//                Text("La zona assignada es: ")
-//                Text(zona.nom)
-//            } }
-//    }
-//}
-//@Composable
-//fun PrecioScreen2(db: Database, controller: NavController){
-//    val preusdb = db.preuQueries.select().executeAsList()
-//    Column {
-//        Nav(controller)
-//        val sessio = remember {
-//            mutableStateOf("")
-//        }
-//        val zona = remember { mutableStateOf("") }
-//        val result = remember { mutableStateOf("") }
-//        Row {
-//            TextField(
-//                value = sessio.value,
-//                placeholder = { Text("Introduce la sessión") },
-//                onValueChange = { sessio.value = it },
-//                modifier = Modifier.fillMaxWidth()
-//            )
-//        }
-//        Row {
-//            TextField(
-//                value = zona.value,
-//                placeholder = { Text("Introduce la zona") },
-//                onValueChange = { zona.value = it },
-//                modifier = Modifier.fillMaxWidth()
-//            )
-//        }
-//
-//        androidx.compose.material3.Button(
-//            onClick = {
-//                val sessioId = sessio.value.toLongOrNull()
-//                val zonaId = zona.value.toLongOrNull()
-//                if(sessioId!=null && zonaId != null){
-//                    val preuEncontrado = preusdb.find { it.sessio==sessioId && it.zona==zonaId }
-//                    result.value = "el precio es: $preuEncontrado"
-//                }
-//            },
-//            modifier = Modifier.padding(vertical = 40.dp)
-//        ) {
-//            androidx.compose.material3.Text("go")
-//        }
-//        Spacer(modifier = Modifier.height(10.dp))
-//
-//        androidx.compose.material3.Text(text = result.value)
-//    }
-//}
-//@Composable
-//fun CompraEntrada(db: Database, controller: NavController){
-//
-//    val entradas = db.entradaQueries.select().executeAsList()
-//
-//    Column {
-//        Nav(controller)
-//
-//        Text("ENTRADAS NO DISPONIBLES:")
-//        Text("Butacas NO DISPONIBLES")
-//        LazyRow {
-//            items(entradas){
-//                    entrada ->
-//                Row(
-//                    modifier = Modifier
-//                        .padding(4.dp)
-//                        .border(1.dp, color = MaterialTheme.colors.primary, CircleShape)
-//                        .padding(4.dp).fillMaxWidth()
-//
-//                ){
-//                    Text(entrada.butaca.toString(),modifier = Modifier.padding(all = 4.dp),style = MaterialTheme.typography.button)
-//                }
-//            } }
-//        Text("Sesiones no disponibles")
-//        LazyRow {
-//            items(entradas){
-//                    entrada ->
-//                Row(
-//                    modifier = Modifier
-//                        .padding(4.dp)
-//                        .border(1.dp, color = MaterialTheme.colors.primary, CircleShape)
-//                        .padding(4.dp).fillMaxWidth()
-//                ){
-//                    Text(entrada.sessio.toString(),modifier = Modifier.padding(all = 4.dp),style = MaterialTheme.typography.button)
-//                }
-//            } }
-//
-//
-//        val sessio = remember {
-//            mutableStateOf("")
-//        }
-//        val butaca = remember { mutableStateOf("") }
-//        val result = remember { mutableStateOf("") }
-//        Row {
-//            TextField(
-//                value = sessio.value,
-//                placeholder = { Text("Introduce la sessio") },
-//                onValueChange = { sessio.value = it },
-//                modifier = Modifier.fillMaxWidth()
-//            )
-//        }
-//        Row {
-//            TextField(
-//                value = butaca.value,
-//                placeholder = { Text("Introduce la butaca") },
-//                onValueChange = { butaca.value = it },
-//                modifier = Modifier.fillMaxWidth()
-//            )
-//        }
-//
-//        androidx.compose.material3.Button(
-//            onClick = {
-//                val sessioId = sessio.value.toLongOrNull()
-//                val butacaId = butaca.value.toLongOrNull()
-//                if(sessioId!=null && butacaId != null){
-//                   // val butacaId= db.butacaQueries.insert(zonaId) algo de la zxona porque no lo inserta bien
-//                    val entrada = db.entradaQueries.insert(butacaId,sessioId)
-//                    if(entrada!= null){
-//                        result.value = "Felicidades has finalizado tu compra"
-//                    }else{
-//                        result.value = "Introduciste mal los datos"
-//                    }
-//
-//                }
-//            },
-//            modifier = Modifier.padding(vertical = 40.dp)
-//        ) {
-//            androidx.compose.material3.Text("go")
-//        }
-//        Spacer(modifier = Modifier.height(10.dp))
-//
-//        androidx.compose.material3.Text(text = result.value)
-//    }
-//}
 
 
     @Composable
@@ -366,4 +170,3 @@ fun ButacaScreen(db: Database, controller: NavController) {
 
         }
     }
-}
